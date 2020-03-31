cb4c94abf597ab578c6544de7e80ca8a54cad316
Sarah0079ja <sodiavbara@gmail.com>
2020-03-31 22:30:45 +0100

    ffff

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -13,7 +13,7 @@ const Landing = ()  => {
    const [CurrentPage, setCurrentPage] = useState([0])
 
   useEffect(() => {
-    const endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=https://image.tmdb.org/t/p/&language=en-US&page=1`;
+    const endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=1b5db8d5068d9319c006d7b0385d2f65&language=en-US&page=1`;
     fetchMovies(endpoint)
   }, [])
 
